e2e310b2e2b3305728ed730ba2e68baa68c9fef5
Uğur Biçer <uuur86@yandex.com>
2019-06-30 01:01:58 +0300

    First demo not tested

---
+++ src/StringObjects.php
@@ -0,0 +1,36 @@
+<?php
+
+class StringObjects {
+	protected $obj = null;
+
+
+
+	public function __construct( $obj ) {
+		$this->obj = $obj;
+	}
+
+
+
+	public function get( $str ) {
+
+		if( empty( $this->obj ) ) return false;
+
+		$str_exp	= explode( '/', $str );
+		$obj		= $this->obj;
+		$val		= null;
+
+		foreach( $str_exp as $obj_name ) {
+
+			if( !isset( $obj->{ $obj_name } ) ) retun false;
+
+			if( empty( $val ) ) {
+				$val = $obj->{ $obj_name };
+			}
+			else {
+				$val = $val->{ $obj_name };
+			}
+		}
+
+		return $val;
+	}
+}
